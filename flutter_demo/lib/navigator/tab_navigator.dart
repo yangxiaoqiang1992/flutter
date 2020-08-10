eda53f5ef27ee8c6bfc2a4454ae8bf8bc3fe632c
yangxq <yangxq@yjhealth.cn>
2020-08-10 09:30:18 +0800

    'flutter代码'

---
+++ flutter_demo/lib/navigator/tab_navigator.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/pages/home_page.dart';
+import 'package:flutter_demo/pages/my_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final PageController _controller = PageController(initialPage: 0);
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.blue;
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[HomePage(), MyPage()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _controller.jumpToPage(index);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: _defaultColor),
+                title: Text('首页'),
+                activeIcon: Icon(Icons.home, color: _activeColor)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle, color: _defaultColor),
+                title: Text('我的'),
+                activeIcon: Icon(Icons.account_circle, color: _activeColor))
+          ]),
+    );
+  }
+}
