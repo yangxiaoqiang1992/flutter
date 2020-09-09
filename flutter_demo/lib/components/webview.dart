dfff1976fd318dd96276780ca77425a4ebcb8058
yangxq <yangxq@yjhealth.cn>
2020-09-09 22:00:27 +0800

    首页添加滚动渐变

---
+++ flutter_demo/lib/components/webview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebView extends StatefulWidget {
+  WebView({Key key}) : super(key: key);
+
+  @override
+  _WebViewState createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('DDD'),
+    );
+  }
+}
--- flutter_demo/lib/pages/home_page.dart
+++ flutter_demo/lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_demo/model/banner_model.dart';
 import 'package:flutter_demo/model/home_model.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
+const double APP_SCROLL_OFFSET = 100;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,6 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin {
   List<BannerModel> _banners = [];
+  double appBarAlpha = 0;
   @override
   void initState() {
     HomeDao.fetch().then((HomeModel model) {
@@ -25,37 +28,66 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            height: 180,
-            child: Stack(children: <Widget>[
-              Swiper(
-                autoplay: true,
-                autoplayDelay: 5000,
-                duration: 1000,
-                itemCount: _banners.length,
-                itemBuilder: (context, index) =>
-                    Image.network(_banners[index].icon, fit: BoxFit.fill),
-                pagination: SwiperPagination(),
-              ),
-              Opacity(
-                opacity: 1,
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(color: Colors.white),
-                  child: Row(
-                    children: <Widget>[
-                      Text('hhh'),
-                      Expanded(
-                          child: TextField(
-                        decoration: InputDecoration(hintText: 'hhh'),
-                      ))
-                    ],
-                  ),
+      body: Stack(children: <Widget>[
+        NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification scrollNotification) {
+            if (scrollNotification is ScrollUpdateNotification &&
+                scrollNotification.depth == 0) {
+              //deep =0 排除其他滚动因素
+              _onscroll(scrollNotification.metrics.pixels);
+            }
+          },
+          child: ListView(children: <Widget>[
+            Container(
+              height: 180,
+              child: Stack(children: <Widget>[
+                Swiper(
+                  autoplay: true,
+                  autoplayDelay: 5000,
+                  duration: 1000,
+                  itemCount: _banners.length,
+                  itemBuilder: (context, index) =>
+                      Image.network(_banners[index].icon, fit: BoxFit.fill),
+                  pagination: SwiperPagination(),
                 ),
-              ),
-            ])));
+              ]),
+            ),
+            Container(height: 800, child: ListTile(title: Text('测试滚动'))),
+          ]),
+        ),
+        Opacity(
+          opacity: appBarAlpha,
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Row(
+              children: <Widget>[
+                Text('hhh'),
+                Expanded(
+                    child: TextField(
+                  decoration: InputDecoration(hintText: 'hhh'),
+                ))
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 
   @override
   bool get wantKeepAlive => true;
+
+  void _onscroll(offset) {
+    double alpha = offset / APP_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
 }
